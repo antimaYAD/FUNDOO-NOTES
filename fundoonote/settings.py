7feff1460a439347f8c504702a097143d0bb0e9f
ANTIMA YADAV <110221648+antimaYAD@users.noreply.github.com>
2024-09-16 16:56:47 +0530

    Update settings.py

--- fundoonote/settings.py
+++ fundoonote/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'  # Define the URL to access static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Define the directory where static files will be collected
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
